f02362bc9d24a23361bca95982061281459e0594
eunjuchoi-estsoft <eunjuchoi@eunjuchoiui-MacBookPro.local>
2025-07-29 15:12:47 +0900

    add: 프로젝트 생성 (#1)

---
+++ SearchOpenLibraryBook/SearchOpenLibraryBook/SearchOpenLibraryBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchOpenLibraryBookApp.swift
+//  SearchOpenLibraryBook
+//
+//  Created by eunjuchoi on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchOpenLibraryBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
